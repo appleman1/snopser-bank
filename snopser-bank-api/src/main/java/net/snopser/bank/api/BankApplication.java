387fbf60d14275577f7c58c722379a56c81a218d
Viktor.Falkenberg <Viktor.Falkenberg@mediascope.net>
2019-04-03 12:49:30 +0300

    Набросал начало

---
+++ snopser-bank-api/src/main/java/net/snopser/bank/api/BankApplication.java
@@ -0,0 +1,14 @@
+package net.snopser.bank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Виктор Фалькенберг (viktor.falkenberg@mediascope.net)
+ */
+@SpringBootApplication
+public class BankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplication.class, args);
+    }
+}
