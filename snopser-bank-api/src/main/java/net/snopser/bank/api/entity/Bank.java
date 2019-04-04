dbdec80b4cbb32a78a34986673c05cbc811875ce
Viktor <vfalkenberg@bk.ru>
2019-04-04 20:47:28 +0300

    Добавлены сущности

---
+++ snopser-bank-api/src/main/java/net/snopser/bank/api/entity/Bank.java
@@ -0,0 +1,33 @@
+package net.snopser.bank.api.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name="banks")
+public class Bank {
+    @Id
+    @Column(name="bank_id")
+    private UUID bankId;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="code")
+    private int code;
+
+    public UUID getBankId() {
+        return bankId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
