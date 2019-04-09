8c0407167ad2262c88e1eb881616f59ea94451f5
Viktor.Falkenberg <Viktor.Falkenberg@mediascope.net>
2019-04-09 14:47:37 +0300

    -

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO accounts (account_id, bank_id, client_id, count)
-values (1111567891,1111,'cbca327ff25841fa876b0e854b2b4dbc', 10000);
+values (1111567891, 3333, 'cbca327ff25841fa876b0e854b2b4dbc', 10000);
