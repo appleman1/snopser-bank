9d7cca167b175514a42dd8ceb1b3fe39dff3a909
Viktor.Falkenberg <Viktor.Falkenberg@mediascope.net>
2019-04-10 12:23:59 +0300

    добавил сервисы пока без логики

---
+++ src/main/java/net/snopser/bank/snopserbank/controller/TransferController.java
@@ -0,0 +1,14 @@
+package net.snopser.bank.snopserbank.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Виктор Фалькенберг (viktor.falkenberg@mediascope.net)
+ */
+
+@RestController
+@RequestMapping("/transfer")
+public class TransferController {
+
+}
