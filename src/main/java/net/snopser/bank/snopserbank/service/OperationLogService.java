04a66165909611bab8798a0a437fde2e5cdaa89b
Viktor.Falkenberg <Viktor.Falkenberg@mediascope.net>
2019-04-11 14:12:10 +0300

    -

--- src/main/java/net/snopser/bank/snopserbank/service/OperationLogService.java
+++ src/main/java/net/snopser/bank/snopserbank/service/OperationLogService.java
@@ -2,9 +2,7 @@ package net.snopser.bank.snopserbank.service;
 
 import net.snopser.bank.snopserbank.entity.OperationLog;
 
-import java.math.BigInteger;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * @author Виктор Фалькенберг (viktor.falkenberg@mediascope.net)
